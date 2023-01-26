86f30436bf079ead92343146f55597afdf8024c7
Samuel Tetteh <khobbylynx55@gmail.com>
2023-01-26 16:58:23 +0000

    fixed webvitals error and changed ico file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
